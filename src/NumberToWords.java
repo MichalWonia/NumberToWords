22babbb1a95afcf156ff6c6adb96bf547c1da598
Michal Wonia <spodek.mw@gmail.com>
2021-11-30 14:05:31 +0100

    Added method NumberToWords to convert given number into words. Project finished.

--- src/NumberToWords.java
+++ src/NumberToWords.java
@@ -15,7 +15,54 @@ public class NumberToWords {
         return String.valueOf(number).length();
     }
 
-    public static void main(String[] args) {
+    public static void numberToWords(int number) {
+        if (number < 0) {
+            System.out.println("Minus");
+            number = -number;
+        }
 
+        int numberLength = getDigitCount(number);
+        int reversedNumber = reverseNumber(number);
+
+        for (int j = 0; j < numberLength; j++) {
+
+            int digits = reversedNumber % 10;
+
+            if (digits == 0) {
+                System.out.println("Zero");
+            }
+            if (digits == 1) {
+                System.out.println("One");
+            }
+            if (digits == 2) {
+                System.out.println("Two");
+            }
+            if (digits == 3) {
+                System.out.println("Three");
+            }
+            if (digits == 4) {
+                System.out.println("Four");
+            }
+            if (digits == 5) {
+                System.out.println("Five");
+            }
+            if (digits == 6) {
+                System.out.println("Six");
+            }
+            if (digits == 7) {
+                System.out.println("Seven");
+            }
+            if (digits == 8) {
+                System.out.println("Eight");
+            }
+            if (digits == 9) {
+                System.out.println("Nine");
+            }
+            reversedNumber = reversedNumber / 10;
+        }
+    }
+
+    public static void main(String[] args) {
+        numberToWords(-1680);
     }
 }
