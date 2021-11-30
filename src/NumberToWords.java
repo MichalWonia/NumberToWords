0d2aef116cd0e37995a0530662ddeb773d5adeed
Michal Wonia <spodek.mw@gmail.com>
2021-11-30 13:43:42 +0100

    Added a method to reverse number.

--- src/NumberToWords.java
+++ src/NumberToWords.java
@@ -1,6 +1,20 @@
 public class NumberToWords {
 
+    public static int reverseNumber(int number) {
+        int reverse = 0;
+        while (number != 0) {
+            int digits = number % 10;
+            reverse = reverse + digits;
+            reverse = reverse * 10;
+            number = number / 10;
+        }
+        return reverse / 10;
+    }
+
+
+
+
     public static void main(String[] args) {
-        
+
     }
 }
